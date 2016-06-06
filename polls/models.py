2a380a58d17bbaa4ba915a665bdfa0401f460e18
kangsgo <347657442@qq.com>
2016-06-06 20:08:58 +0800

    add date

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+#add Question data
+class Question(models.Model):
+	"""question_text:Text
+	pub_date:Date"""
+	question_text=models.CharField(max_length=200)
+	pub_date=models.DateField('date published')
+
+#add Choice date
+class Choice(models.Model):
+	"""Question relate question"""
+	question=models.ForeignKey(Question)
+	Choice_text=models.CharField(max_length=200)
+	votes=models.IntegerField(default=0)
