8a56c20a63f89a622d443bd92f60b243cd2f7212
kangsgo <347657442@qq.com>
2016-06-06 20:42:08 +0800

    part2

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Question,Choice
 
 # Register your models here.
+#set Choice
+class ChoiceInline(admin.TabularInline):
+	"""docstring for ChoiceInline"""
+	model =Choice
+	extra=3
+
+
+#set Question
+class QuestionAdmin(admin.ModelAdmin):
+	"""docstring for QuestionAdmin"""
+	fieldsets=[
+	     (None,                         {'fields':['question_text']}),
+	     ('Date information', {'fields':['pub_date'], 'classes':['collapse']}),
+	]
+	inlines = [ChoiceInline]
+
+
+
+admin.site.register(Question,QuestionAdmin)
--- polls/models.py
+++ polls/models.py
@@ -8,6 +8,9 @@ class Question(models.Model):
 	pub_date:Date"""
 	question_text=models.CharField(max_length=200)
 	pub_date=models.DateField('date published')
+	def  __unicode__(self):
+		return self.question_text
+
 
 #add Choice date
 class Choice(models.Model):
@@ -15,3 +18,5 @@ class Choice(models.Model):
 	question=models.ForeignKey(Question)
 	Choice_text=models.CharField(max_length=200)
 	votes=models.IntegerField(default=0)
+	def __unicode__(self):
+		return self.Choice_text
